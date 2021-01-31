4dc83e9169c83910a02fbdacb0ee29a2e87f5a7d
Bonet <Watafackah@gmail.com>
2021-01-31 21:24:39 +0100

    SingletonFinished

---
+++ PrototypeProg/Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public Text ValueText;
+
+    private void Start()
+    {
+        ValueText.text = Singleton.Instance.Value.ToString();
+    }
+
+    public void GoToFirstScene()
+    {
+        SceneManager.LoadScene("Primera");
+        Singleton.Instance.Value++;
+    }
+
+    public void GoToSecondScene()
+    {
+        SceneManager.LoadScene("Segunda");
+        Singleton.Instance.Value++;
+    }
+}
---
+++ PrototypeProg/Assets/Scripts/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Singleton : MonoBehaviour
+{
+    public static Singleton Instance { get; private set; }
+
+    public int Value;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
